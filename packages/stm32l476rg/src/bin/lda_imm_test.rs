579d2536c397de22123007d20f4081aaf1ce16e0
YSawc <niceage6690@gmail.com>
2024-01-12 08:29:22 +0900

    update: add addressing starting after instruction mode read.

--- packages/stm32l476rg/src/bin/lda_imm_test.rs
+++ packages/stm32l476rg/src/bin/lda_imm_test.rs
@@ -21,13 +21,14 @@ fn main() -> ! {
     buf[0] = CpuMode::Debug as u8;
     buf[1] = OpeMode::Inst as u8;
     buf[2] = 0xa9;
+    buf[3] = 0x34;
     unwrap!(usart.blocking_write(&buf));
     info!("wrote mode and additional data.");
     loop {
         match usart.blocking_read(&mut buf) {
             Ok(_) => {
                 match buf.first().unwrap() {
-                    0x27 => info!("test passed!"),
+                    0x34 => info!("test passed!"),
                     v => info!("test failed. return value is {:?}", v),
                 }
                 info!("wait kill..");
