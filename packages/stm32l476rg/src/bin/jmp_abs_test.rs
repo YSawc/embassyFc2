3cf995f5f529eaf91d5a73078b28b323a3799e25
YSawc <niceage6690@gmail.com>
2024-01-22 21:27:24 +0900

    update: apply rts to callback 0x4c instruction.

--- packages/stm32l476rg/src/bin/jmp_abs_test.rs
+++ packages/stm32l476rg/src/bin/jmp_abs_test.rs
@@ -9,23 +9,78 @@ use embassy_stm32::{bind_interrupts, peripherals, usart};
 use {defmt_rtt as _, panic_probe as _};
 
 bind_interrupts!(struct Irqs {
-    UART4 => usart::InterruptHandler<peripherals::UART4>;
+    USART1 => usart::InterruptHandler<peripherals::USART1>;
 });
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
     let p = embassy_stm32::init(Default::default());
     let config = Config::default();
-    let mut usart = Uart::new(p.UART4, p.PA1, p.PA0, Irqs, NoDma, NoDma, config).unwrap();
+    let mut usart = Uart::new_with_rtscts(
+        p.USART1, p.PA10, p.PA9, Irqs, p.PA12, p.PA11, NoDma, NoDma, config,
+    )
+    .unwrap();
+
     let mut write_buf1 = [0x0u8; 5];
     write_buf1[0] = CpuMode::Debug as u8;
     write_buf1[1] = OpeMode::Inst as u8;
     write_buf1[2] = 0x4c;
     write_buf1[3] = 0xf5;
     write_buf1[4] = 0xc5;
-    unwrap!(usart.blocking_write(&write_buf1));
+
+    let mut buf = [0x0u8; 1];
+    buf[0] = CpuMode::Debug as u8;
+    'blocking_write_cpu_mode: loop {
+        match usart.blocking_write(&buf) {
+            Ok(_) => {
+                info!("write cpu operation mode.");
+                break 'blocking_write_cpu_mode;
+            }
+            Err(e) => info!("error while writing: {}", e),
+        }
+    }
+    buf[0] = OpeMode::Inst as u8;
+    'blocking_write_operation: loop {
+        match usart.blocking_write(&buf) {
+            Ok(_) => {
+                info!("write operation mode.");
+                break 'blocking_write_operation;
+            }
+            Err(e) => info!("error while writing: {}", e),
+        }
+    }
+    buf[0] = 0x4c;
+    'blocking_write_instruction: loop {
+        match usart.blocking_write(&buf) {
+            Ok(_) => {
+                info!("write instruction.");
+                break 'blocking_write_instruction;
+            }
+            Err(e) => info!("error while writing: {}", e),
+        }
+    }
+    buf[0] = 0xf5;
+    'blocking_write_row: loop {
+        match usart.blocking_write(&buf) {
+            Ok(_) => {
+                info!("write row data.");
+                break 'blocking_write_row;
+            }
+            Err(e) => info!("error while writing: {}", e),
+        }
+    }
+    buf[0] = 0xc5;
+    'blocking_write_high: loop {
+        match usart.blocking_write(&buf) {
+            Ok(_) => {
+                info!("write high data.");
+                break 'blocking_write_high;
+            }
+            Err(e) => info!("error while writing: {}", e),
+        }
+    }
+
     let mut read_buf = [0x0u8; 2];
-    info!("wrote 0xa9 instruction and imm data.");
     loop {
         match usart.blocking_read(&mut read_buf) {
             Ok(_) => {
