76eba614392199247c28997ca422723324c8a2dd
YSawc <niceage6690@gmail.com>
2024-02-05 05:29:36 +0900

    modify: modify test executor

---
+++ packages/test-executor/src/main.rs
@@ -0,0 +1,29 @@
+use std::{env, path::Path, process::Command};
+
+fn exe_testcase(testcase: String, timeout: u8) {
+    let root = Path::new("../stm32l476rg/");
+    assert!(env::set_current_dir(&root).is_ok());
+    match Command::new("timeout")
+        .arg(format!("{}", timeout))
+        .arg("cargo")
+        .arg("run")
+        .arg("--bin")
+        .arg("jmp_abs_test")
+        .output()
+    {
+        Ok(output) => {
+            if !std::str::from_utf8(&output.stdout)
+                .unwrap()
+                .contains("test pass")
+            {
+                panic!("jmp_abs_test failed.");
+            }
+        }
+        Err(e) => println!("child error occured. {}", e),
+    }
+    println!("{} passed.", testcase);
+}
+
+fn main() {
+    exe_testcase("jmp_abs_test".to_string(), 8);
+}
