082e83f9738ecd8c1d9edb790e120585ce2a48e2
YSawc <niceage6690@gmail.com>
2024-02-18 07:27:58 +0900

    chore: add carry test for 0xFE

--- packages/stm32l476rg/src/bin/inc_absx_test.rs
+++ packages/stm32l476rg/src/bin/inc_absx_test.rs
@@ -45,36 +45,36 @@ pub fn test_inc_abs_x_without_carry<T: BasicInstance, P: Pin>(usart: &mut Uart<T
     check_valid_register_status(usart, TxReg::P, &[0b00000000]);
 }
 
-// pub fn test_inc_abs_x_with_carry<T: BasicInstance, P: Pin>(usart: &mut Uart<T>, nop: &Input<P>) {
-//     send_reset_signal_if_not_nop(usart, &nop);
-//     usart.blocking_write(&[CpuMode::Debug as u8]).unwrap();
-//
-//     // store 0xff to x
-//     usart_write(usart, &[OpeMode::Inst as u8, 0xa2, 0xff]);
-//     check_valid_register_status(usart, TxReg::P, &[0b10000000]);
-//
-//     usart_write(usart, &[OpeMode::Inst as u8, 0xFE, 0x30, 0x40]);
-//     let mut read_buf = [0x0u8; 2];
-//     usart.blocking_read(&mut read_buf).unwrap();
-//     match read_buf {
-//         [0x2F, 0x41] => info!("6502 access valid memory!"),
-//         v => {
-//             info!("test failed. return value is {:?}", v);
-//             loop {}
-//         }
-//     }
-//     usart_write(usart, &[0x60]);
-//     let mut data_buf = [0x0u8; 1];
-//     usart.blocking_read(&mut data_buf).unwrap();
-//     match data_buf {
-//         [0x61] => info!("receive valid data."),
-//         v => {
-//             info!("test failed. return value is {:?}", v);
-//             loop {}
-//         }
-//     }
-//     check_valid_register_status(usart, TxReg::P, &[0b10000001]);
-// }
+pub fn test_inc_abs_x_with_carry<T: BasicInstance, P: Pin>(usart: &mut Uart<T>, nop: &Input<P>) {
+    send_reset_signal_if_not_nop(usart, &nop);
+    usart.blocking_write(&[CpuMode::Debug as u8]).unwrap();
+
+    // store 0xff to x
+    usart_write(usart, &[OpeMode::Inst as u8, 0xa2, 0xff]);
+    check_valid_register_status(usart, TxReg::P, &[0b10000000]);
+
+    usart_write(usart, &[OpeMode::Inst as u8, 0xFE, 0x30, 0x40]);
+    let mut read_buf = [0x0u8; 2];
+    usart.blocking_read(&mut read_buf).unwrap();
+    match read_buf {
+        [0x2F, 0x41] => info!("6502 access valid memory!"),
+        v => {
+            info!("test failed. return value is {:?}", v);
+            loop {}
+        }
+    }
+    usart_write(usart, &[0x60]);
+    let mut data_buf = [0x0u8; 1];
+    usart.blocking_read(&mut data_buf).unwrap();
+    match data_buf {
+        [0x61] => info!("receive valid data."),
+        v => {
+            info!("test failed. return value is {:?}", v);
+            loop {}
+        }
+    }
+    check_valid_register_status(usart, TxReg::P, &[0b00000001]);
+}
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
@@ -87,7 +87,7 @@ fn main() -> ! {
     let _rw = Input::new(p.PA0, Pull::None);
     let nop = Input::new(p.PA1, Pull::None);
     test_inc_abs_x_without_carry(&mut usart, &nop);
-    // test_inc_abs_x_with_carry(&mut usart, &nop);
+    test_inc_abs_x_with_carry(&mut usart, &nop);
 
     info!("test passed!");
     loop {}
