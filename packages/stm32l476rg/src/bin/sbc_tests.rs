ba97a4475bf9a6eb2a69526c0c3c86c6f2b2c5b5
YSawc <niceage6690@gmail.com>
2024-02-27 07:06:32 +0900

    modify: modify 0xE1, 0xE5, 0xED, 0xF1, 0xF5, 0xF9, 0xFD tests

--- packages/stm32l476rg/src/bin/sbc_tests.rs
+++ packages/stm32l476rg/src/bin/sbc_tests.rs
@@ -14,6 +14,50 @@ bind_interrupts!(struct Irqs {
     USART1 => usart::InterruptHandler<peripherals::USART1>;
 });
 
+pub fn test_sbc_indx<T: BasicInstance, P: Pin, P2: Pin>(
+    usart: &mut Uart<T>,
+    nop: &Input<P>,
+    resb: &mut Output<P2>,
+) {
+    send_reset_signal_if_not_nop(&nop, resb);
+    usart_write(usart, &[CpuMode::Debug as u8]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xC9, 0x00]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000011]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xA9, 0x40]);
+    check_valid_register_status(usart, TxReg::A, &[0x40]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000001]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xE1, 0x80]);
+    usart_read_with_check(usart, &mut [0x0u8; 2], &[0x80, 0x00]);
+    usart.blocking_write(&[0x00]).unwrap();
+    usart_read_with_check(usart, &mut [0x0u8; 2], &[0x81, 0x00]);
+    usart.blocking_write(&[0x02]).unwrap();
+    usart_read_with_check(usart, &mut [0x0u8; 2], &[0x00, 0x02]);
+    usart.blocking_write(&[0x41]).unwrap();
+    check_valid_register_status(usart, TxReg::A, &[0xFF]);
+    check_valid_register_status(usart, TxReg::P, &[0b10000000]);
+    info!("test_sbc_indx passed!");
+}
+
+pub fn test_sbc_zp<T: BasicInstance, P: Pin, P2: Pin>(
+    usart: &mut Uart<T>,
+    nop: &Input<P>,
+    resb: &mut Output<P2>,
+) {
+    send_reset_signal_if_not_nop(&nop, resb);
+    usart_write(usart, &[CpuMode::Debug as u8]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xC9, 0x00]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000011]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xA9, 0x40]);
+    check_valid_register_status(usart, TxReg::A, &[0x40]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000001]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xE5, 0x78]);
+    usart_read_with_check(usart, &mut [0x0u8; 2], &[0x78, 0x00]);
+    usart.blocking_write(&[0x3F]).unwrap();
+    check_valid_register_status(usart, TxReg::A, &[0x01]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000001]);
+    info!("test_sbc_zp passed!");
+}
+
 pub fn test_sbc_imm_without_carry<T: BasicInstance, P: Pin, P2: Pin>(
     usart: &mut Uart<T>,
     nop: &Input<P>,
@@ -27,7 +71,7 @@ pub fn test_sbc_imm_without_carry<T: BasicInstance, P: Pin, P2: Pin>(
     usart_write(usart, &[OpeMode::Inst as u8, 0xE9, 0x00]);
     check_valid_register_status(usart, TxReg::A, &[0x7F]);
     check_valid_register_status(usart, TxReg::P, &[0b01000001]);
-    info!("test_sbcimm_without_carry passed!");
+    info!("test_sbc_imm_without_carry passed!");
 }
 
 pub fn test_sbc_imm_with_carry<T: BasicInstance, P: Pin, P2: Pin>(
@@ -65,6 +109,117 @@ pub fn test_sbc_imm_with_overflow<T: BasicInstance, P: Pin, P2: Pin>(
     check_valid_register_status(usart, TxReg::P, &[0b10000000]);
     info!("test_sbc_imm_with_overflow passed!");
 }
+
+pub fn test_sbc_abs<T: BasicInstance, P: Pin, P2: Pin>(
+    usart: &mut Uart<T>,
+    nop: &Input<P>,
+    resb: &mut Output<P2>,
+) {
+    send_reset_signal_if_not_nop(&nop, resb);
+    usart_write(usart, &[CpuMode::Debug as u8]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xC9, 0x00]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000011]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xA9, 0x40]);
+    check_valid_register_status(usart, TxReg::A, &[0x40]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000001]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xED, 0x78, 0x06]);
+    usart_read_with_check(usart, &mut [0x0u8; 2], &[0x78, 0x06]);
+    usart.blocking_write(&[0x3F]).unwrap();
+    check_valid_register_status(usart, TxReg::A, &[0x01]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000001]);
+    info!("test_sbc_abs passed!");
+}
+
+pub fn test_sbc_indy<T: BasicInstance, P: Pin, P2: Pin>(
+    usart: &mut Uart<T>,
+    nop: &Input<P>,
+    resb: &mut Output<P2>,
+) {
+    send_reset_signal_if_not_nop(&nop, resb);
+    usart_write(usart, &[CpuMode::Debug as u8]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xC9, 0x00]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000011]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xA9, 0x40]);
+    check_valid_register_status(usart, TxReg::A, &[0x40]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000001]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xF1, 0x33]);
+    usart_read_with_check(usart, &mut [0x0u8; 2], &[0x33, 0x00]);
+    usart.blocking_write(&[0x00]).unwrap();
+    usart_read_with_check(usart, &mut [0x0u8; 2], &[0x34, 0x00]);
+    usart.blocking_write(&[0x04]).unwrap();
+    usart_read_with_check(usart, &mut [0x0u8; 2], &[0x00, 0x04]);
+    usart.blocking_write(&[0x40]).unwrap();
+    check_valid_register_status(usart, TxReg::A, &[0x00]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000011]);
+    info!("test_sbc_indy passed!");
+}
+
+pub fn test_sbc_zpx<T: BasicInstance, P: Pin, P2: Pin>(
+    usart: &mut Uart<T>,
+    nop: &Input<P>,
+    resb: &mut Output<P2>,
+) {
+    send_reset_signal_if_not_nop(&nop, resb);
+    usart_write(usart, &[CpuMode::Debug as u8]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xC9, 0x00]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000011]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xA9, 0x40]);
+    check_valid_register_status(usart, TxReg::A, &[0x40]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000001]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xA2, 0x78]);
+    check_valid_register_status(usart, TxReg::X, &[0x78]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000001]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xF5, 0x00]);
+    usart_read_with_check(usart, &mut [0x0u8; 2], &[0x78, 0x00]);
+    usart.blocking_write(&[0x41]).unwrap();
+    check_valid_register_status(usart, TxReg::A, &[0xFF]);
+    check_valid_register_status(usart, TxReg::P, &[0b10000000]);
+    info!("test_sbc_zpx passed!");
+}
+
+pub fn test_sbc_absy<T: BasicInstance, P: Pin, P2: Pin>(
+    usart: &mut Uart<T>,
+    nop: &Input<P>,
+    resb: &mut Output<P2>,
+) {
+    send_reset_signal_if_not_nop(&nop, resb);
+    usart_write(usart, &[CpuMode::Debug as u8]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xC9, 0x00]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000011]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xA9, 0x81]);
+    check_valid_register_status(usart, TxReg::A, &[0x81]);
+    check_valid_register_status(usart, TxReg::P, &[0b10000001]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xF9, 0x00, 0x04]);
+    usart_read_with_check(usart, &mut [0x0u8; 2], &[0x00, 0x04]);
+    usart.blocking_write(&[0x7F]).unwrap();
+    check_valid_register_status(usart, TxReg::A, &[0x02]);
+    check_valid_register_status(usart, TxReg::P, &[0b01000001]);
+    info!("test_sbc_absy passed!");
+}
+
+pub fn test_sbc_absx<T: BasicInstance, P: Pin, P2: Pin>(
+    usart: &mut Uart<T>,
+    nop: &Input<P>,
+    resb: &mut Output<P2>,
+) {
+    send_reset_signal_if_not_nop(&nop, resb);
+    usart_write(usart, &[CpuMode::Debug as u8]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xC9, 0x00]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000011]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xA2, 0x78]);
+    check_valid_register_status(usart, TxReg::X, &[0x78]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000001]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xA9, 0x40]);
+    check_valid_register_status(usart, TxReg::A, &[0x40]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000001]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xFD, 0x00, 0x06]);
+    usart_read_with_check(usart, &mut [0x0u8; 2], &[0x78, 0x06]);
+    usart.blocking_write(&[0x40]).unwrap();
+    check_valid_register_status(usart, TxReg::A, &[0x00]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000011]);
+    info!("test_sbc_absx passed!");
+}
+
 #[cortex_m_rt::entry]
 fn main() -> ! {
     let p = embassy_stm32::init(Default::default());
@@ -76,9 +231,16 @@ fn main() -> ! {
     let _rw = Input::new(p.PA0, Pull::None);
     let nop = Input::new(p.PA1, Pull::None);
     let mut resb = Output::new(p.PA4, Level::Low, Speed::Medium);
+    test_sbc_indx(&mut usart, &nop, &mut resb);
+    test_sbc_zp(&mut usart, &nop, &mut resb);
     test_sbc_imm_without_carry(&mut usart, &nop, &mut resb);
     test_sbc_imm_with_carry(&mut usart, &nop, &mut resb);
     test_sbc_imm_with_overflow(&mut usart, &nop, &mut resb);
+    test_sbc_abs(&mut usart, &nop, &mut resb);
+    test_sbc_indy(&mut usart, &nop, &mut resb);
+    test_sbc_zpx(&mut usart, &nop, &mut resb);
+    test_sbc_absy(&mut usart, &nop, &mut resb);
+    test_sbc_absx(&mut usart, &nop, &mut resb);
     info!("all tests passed!");
     loop {}
 }
