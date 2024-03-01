ac5ae122c9dd7a2c9a808d55cde7ebc3e933c982
YSawc <niceage6690@gmail.com>
2024-03-01 20:50:44 +0900

    modify: modify 0x28, 0x68

--- packages/stm32l476rg/src/bin/pl_tests.rs
+++ packages/stm32l476rg/src/bin/pl_tests.rs
@@ -14,58 +14,32 @@ bind_interrupts!(struct Irqs {
     USART1 => usart::InterruptHandler<peripherals::USART1>;
 });
 
-pub fn test_php_impl_within_n<T: BasicInstance, P: Pin, P2: Pin>(
+pub fn test_plp_impl<T: BasicInstance, P: Pin, P2: Pin>(
     usart: &mut Uart<T>,
     nop: &Input<P>,
     resb: &mut Output<P2>,
 ) {
     send_reset_signal_if_not_nop(&nop, resb);
     usart_write(usart, &[CpuMode::Debug as u8]);
-    usart_write(usart, &[OpeMode::Inst as u8, 0xA9, 0x80]);
-    check_valid_register_status(usart, TxReg::A, &[0x80]);
-    check_valid_register_status(usart, TxReg::P, &[0b10000000]);
-    check_valid_register_status(usart, TxReg::S, &[0xFF]);
-    usart_write(usart, &[OpeMode::Inst as u8, 0x08]);
-    usart_read_with_check(usart, &mut [0x0u8; 3], &[0xFE, 0x00, 0b10000000]);
-    check_valid_register_status(usart, TxReg::A, &[0x80]);
-    check_valid_register_status(usart, TxReg::S, &[0xFE]);
-    info!("test_php_impl_within_n passed!");
-}
-
-pub fn test_php_impl_within_cz<T: BasicInstance, P: Pin, P2: Pin>(
-    usart: &mut Uart<T>,
-    nop: &Input<P>,
-    resb: &mut Output<P2>,
-) {
-    send_reset_signal_if_not_nop(&nop, resb);
-    usart_write(usart, &[CpuMode::Debug as u8]);
-    usart_write(usart, &[OpeMode::Inst as u8, 0xA9, 0x80]);
-    check_valid_register_status(usart, TxReg::A, &[0x80]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xA9, 0x82]);
+    check_valid_register_status(usart, TxReg::A, &[0x82]);
     usart_write(usart, &[OpeMode::Inst as u8, 0xCD, 0x78, 0x06]);
     usart_read_with_check(usart, &mut [0x0u8; 2], &[0x78, 0x06]);
-    usart.blocking_write(&[0x80]).unwrap();
+    usart.blocking_write(&[0x82]).unwrap();
     check_valid_register_status(usart, TxReg::P, &[0b00000011]);
     check_valid_register_status(usart, TxReg::S, &[0xFF]);
     usart_write(usart, &[OpeMode::Inst as u8, 0x08]);
     usart_read_with_check(usart, &mut [0x0u8; 3], &[0xFE, 0x00, 0b00000011]);
     check_valid_register_status(usart, TxReg::S, &[0xFE]);
-    info!("test_php_impl_within_cz passed!");
+    usart_write(usart, &[OpeMode::Inst as u8, 0x28]);
+    usart_read_with_check(usart, &mut [0x0u8; 2], &[0xFE, 0x00]);
+    usart.blocking_write(&[0b00000011]).unwrap();
+    check_valid_register_status(usart, TxReg::P, &[0b00000011]);
+    check_valid_register_status(usart, TxReg::S, &[0xFF]);
+    info!("test_plp_impl passed!");
 }
 
-pub fn test_php_impl_within_none_flag<T: BasicInstance, P: Pin, P2: Pin>(
-    usart: &mut Uart<T>,
-    nop: &Input<P>,
-    resb: &mut Output<P2>,
-) {
-    send_reset_signal_if_not_nop(&nop, resb);
-    usart_write(usart, &[CpuMode::Debug as u8]);
-    usart_write(usart, &[OpeMode::Inst as u8, 0x08]);
-    usart_read_with_check(usart, &mut [0x0u8; 3], &[0xFE, 0x00, 0b00000000]);
-    check_valid_register_status(usart, TxReg::S, &[0xFE]);
-    info!("test_php_impl_within_none_flag passed!");
-}
-
-pub fn test_pha_impl<T: BasicInstance, P: Pin, P2: Pin>(
+pub fn test_pla_impl<T: BasicInstance, P: Pin, P2: Pin>(
     usart: &mut Uart<T>,
     nop: &Input<P>,
     resb: &mut Output<P2>,
@@ -82,10 +56,15 @@ pub fn test_pha_impl<T: BasicInstance, P: Pin, P2: Pin>(
     usart_write(usart, &[OpeMode::Inst as u8, 0x48]);
     usart_read_with_check(usart, &mut [0x0u8; 3], &[0xFE, 0x00, 0x82]);
     check_valid_register_status(usart, TxReg::S, &[0xFE]);
-    usart_write(usart, &[OpeMode::Inst as u8, 0x48]);
-    usart_read_with_check(usart, &mut [0x0u8; 3], &[0xFD, 0x00, 0x82]);
-    check_valid_register_status(usart, TxReg::S, &[0xFD]);
-    info!("test_pha_impl passed!");
+    usart_write(usart, &[OpeMode::Inst as u8, 0xA9, 0x40]);
+    check_valid_register_status(usart, TxReg::A, &[0x40]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0x68]);
+    usart_read_with_check(usart, &mut [0x0u8; 2], &[0xFE, 0x00]);
+    usart.blocking_write(&[0x82]).unwrap();
+    check_valid_register_status(usart, TxReg::A, &[0x82]);
+    check_valid_register_status(usart, TxReg::P, &[0b10000001]);
+    check_valid_register_status(usart, TxReg::S, &[0xFF]);
+    info!("test_pla_impl passed!");
 }
 
 #[cortex_m_rt::entry]
@@ -99,10 +78,8 @@ fn main() -> ! {
     let _rw = Input::new(p.PA0, Pull::None);
     let nop = Input::new(p.PA1, Pull::None);
     let mut resb = Output::new(p.PA4, Level::Low, Speed::Medium);
-    test_php_impl_within_n(&mut usart, &nop, &mut resb);
-    test_php_impl_within_cz(&mut usart, &nop, &mut resb);
-    test_php_impl_within_none_flag(&mut usart, &nop, &mut resb);
-    test_pha_impl(&mut usart, &nop, &mut resb);
+    test_plp_impl(&mut usart, &nop, &mut resb);
+    test_pla_impl(&mut usart, &nop, &mut resb);
     info!("all tests passed!");
     loop {}
 }
