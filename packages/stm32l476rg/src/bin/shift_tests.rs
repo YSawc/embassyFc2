6acff8bbdf8edb32bfe7da682c387416d87492f8
YSawc <niceage6690@gmail.com>
2024-03-03 15:11:56 +0900

    modify: modify 0x26, 0x2A, 0x2E, 0x36, 0x3E

--- packages/stm32l476rg/src/bin/shift_tests.rs
+++ packages/stm32l476rg/src/bin/shift_tests.rs
@@ -127,6 +127,133 @@ pub fn test_asl_absx<T: BasicInstance, P: Pin, P2: Pin>(
     info!("test_asl_absx passed!");
 }
 
+pub fn test_rol_zp<T: BasicInstance, P: Pin, P2: Pin>(
+    usart: &mut Uart<T>,
+    nop: &Input<P>,
+    resb: &mut Output<P2>,
+) {
+    send_reset_signal_if_not_nop(&nop, resb);
+    usart_write(usart, &[CpuMode::Debug as u8]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0x26, 0x78]);
+    usart_read_with_check(usart, &mut [0x0u8; 2], &[0x78, 0x00]);
+    usart.blocking_write(&[0x55]).unwrap();
+    usart_read_with_check(usart, &mut [0x0u8; 1], &[0xAA]);
+    check_valid_register_status(usart, TxReg::P, &[0b10000000]);
+    info!("test_rol_zp passed!");
+}
+
+pub fn test_rol_acc_without_flag<T: BasicInstance, P: Pin, P2: Pin>(
+    usart: &mut Uart<T>,
+    nop: &Input<P>,
+    resb: &mut Output<P2>,
+) {
+    send_reset_signal_if_not_nop(&nop, resb);
+    usart_write(usart, &[CpuMode::Debug as u8]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xA9, 0x20]);
+    check_valid_register_status(usart, TxReg::A, &[0x20]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000000]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0x2A]);
+    check_valid_register_status(usart, TxReg::A, &[0x40]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000000]);
+    info!("test_rol_acc_without_flag passed!");
+}
+
+pub fn test_rol_acc_with_c<T: BasicInstance, P: Pin, P2: Pin>(
+    usart: &mut Uart<T>,
+    nop: &Input<P>,
+    resb: &mut Output<P2>,
+) {
+    send_reset_signal_if_not_nop(&nop, resb);
+    usart_write(usart, &[CpuMode::Debug as u8]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xA9, 0x80]);
+    check_valid_register_status(usart, TxReg::A, &[0x80]);
+    check_valid_register_status(usart, TxReg::P, &[0b10000000]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0x2A]);
+    check_valid_register_status(usart, TxReg::A, &[0x01]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000001]);
+    info!("test_rol_acc_with_c passed!");
+}
+
+pub fn test_rol_acc_with_z<T: BasicInstance, P: Pin, P2: Pin>(
+    usart: &mut Uart<T>,
+    nop: &Input<P>,
+    resb: &mut Output<P2>,
+) {
+    send_reset_signal_if_not_nop(&nop, resb);
+    usart_write(usart, &[CpuMode::Debug as u8]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0x2A]);
+    check_valid_register_status(usart, TxReg::A, &[0x00]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000010]);
+    info!("test_rol_acc_with_z passed!");
+}
+
+pub fn test_rol_acc_with_n<T: BasicInstance, P: Pin, P2: Pin>(
+    usart: &mut Uart<T>,
+    nop: &Input<P>,
+    resb: &mut Output<P2>,
+) {
+    send_reset_signal_if_not_nop(&nop, resb);
+    usart_write(usart, &[CpuMode::Debug as u8]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xA9, 0x40]);
+    check_valid_register_status(usart, TxReg::A, &[0x40]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000000]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0x2A]);
+    check_valid_register_status(usart, TxReg::A, &[0x80]);
+    check_valid_register_status(usart, TxReg::P, &[0b10000000]);
+    info!("test_rol_acc_with_n passed!");
+}
+
+pub fn test_rol_abs<T: BasicInstance, P: Pin, P2: Pin>(
+    usart: &mut Uart<T>,
+    nop: &Input<P>,
+    resb: &mut Output<P2>,
+) {
+    send_reset_signal_if_not_nop(&nop, resb);
+    usart_write(usart, &[CpuMode::Debug as u8]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0x2E, 0x78, 0x26]);
+    usart_read_with_check(usart, &mut [0x0u8; 2], &[0x78, 0x26]);
+    usart.blocking_write(&[0x55]).unwrap();
+    usart_read_with_check(usart, &mut [0x0u8; 1], &[0xAA]);
+    check_valid_register_status(usart, TxReg::P, &[0b10000000]);
+    info!("test_rol_abs passed!");
+}
+
+pub fn test_rol_zpx<T: BasicInstance, P: Pin, P2: Pin>(
+    usart: &mut Uart<T>,
+    nop: &Input<P>,
+    resb: &mut Output<P2>,
+) {
+    send_reset_signal_if_not_nop(&nop, resb);
+    usart_write(usart, &[CpuMode::Debug as u8]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xA2, 0x55]);
+    check_valid_register_status(usart, TxReg::X, &[0x55]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000000]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0x36, 0x00]);
+    usart_read_with_check(usart, &mut [0x0u8; 2], &[0x55, 0x00]);
+    usart.blocking_write(&[0x80]).unwrap();
+    usart_read_with_check(usart, &mut [0x0u8; 1], &[0x01]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000001]);
+    info!("test_rol_zpx passed!");
+}
+
+pub fn test_rol_absx<T: BasicInstance, P: Pin, P2: Pin>(
+    usart: &mut Uart<T>,
+    nop: &Input<P>,
+    resb: &mut Output<P2>,
+) {
+    send_reset_signal_if_not_nop(&nop, resb);
+    usart_write(usart, &[CpuMode::Debug as u8]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xA2, 0x55]);
+    check_valid_register_status(usart, TxReg::X, &[0x55]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000000]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0x3E, 0x00, 0x06]);
+    usart_read_with_check(usart, &mut [0x0u8; 2], &[0x55, 0x06]);
+    usart.blocking_write(&[0x80]).unwrap();
+    usart_read_with_check(usart, &mut [0x0u8; 1], &[0x01]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000001]);
+    info!("test_rol_absx passed!");
+}
+
 #[cortex_m_rt::entry]
 fn main() -> ! {
     let p = embassy_stm32::init(Default::default());
@@ -145,6 +272,14 @@ fn main() -> ! {
     test_asl_abs(&mut usart, &nop, &mut resb);
     test_asl_zpx(&mut usart, &nop, &mut resb);
     test_asl_absx(&mut usart, &nop, &mut resb);
+    test_rol_zp(&mut usart, &nop, &mut resb);
+    test_rol_acc_without_flag(&mut usart, &nop, &mut resb);
+    test_rol_acc_with_c(&mut usart, &nop, &mut resb);
+    test_rol_acc_with_z(&mut usart, &nop, &mut resb);
+    test_rol_acc_with_n(&mut usart, &nop, &mut resb);
+    test_rol_abs(&mut usart, &nop, &mut resb);
+    test_rol_zpx(&mut usart, &nop, &mut resb);
+    test_rol_absx(&mut usart, &nop, &mut resb);
     info!("all tests passed!");
     loop {}
 }
