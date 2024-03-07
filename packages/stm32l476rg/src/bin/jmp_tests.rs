83bdd60097d1002bee1d192c8603f0dd1cfb37d6
YSawc <niceage6690@gmail.com>
2024-03-07 14:08:34 +0900

    modify: modify 0x20

--- packages/stm32l476rg/src/bin/jmp_tests.rs
+++ packages/stm32l476rg/src/bin/jmp_tests.rs
@@ -14,6 +14,25 @@ bind_interrupts!(struct Irqs {
     USART1 => usart::InterruptHandler<peripherals::USART1>;
 });
 
+pub fn test_jsr_abs<T: BasicInstance, P: Pin, P2: Pin>(
+    usart: &mut Uart<T>,
+    nop: &Input<P>,
+    resb: &mut Output<P2>,
+) {
+    send_reset_signal_if_not_nop(&nop, resb);
+    usart_write(usart, &[CpuMode::Debug as u8]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0x4C, 0xF5, 0xC5]);
+    check_valid_register_status(usart, TxReg::PC, &[0xF5, 0xc5]);
+    check_valid_register_status(usart, TxReg::S, &[0xFF]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0x20, 0x2D, 0xC7]);
+    usart_read_with_check(usart, &mut [0x0u8; 3], &[0xFF, 0x01, 0xC5]);
+    usart_read_with_check(usart, &mut [0x0u8; 3], &[0xFE, 0x01, 0xF8]);
+    check_valid_register_status(usart, TxReg::PC, &[0x2D, 0xC7]);
+    check_valid_register_status(usart, TxReg::S, &[0xFD]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000000]);
+    info!("test_jsr_abs passed!");
+}
+
 pub fn test_jmp_abs<T: BasicInstance, P: Pin, P2: Pin, P3: Pin>(
     usart: &mut Uart<T>,
     nop: &Input<P>,
@@ -59,6 +78,7 @@ fn main() -> ! {
     let rw = Input::new(p.PA0, Pull::None);
     let nop = Input::new(p.PA1, Pull::None);
     let mut resb = Output::new(p.PA4, Level::Low, Speed::Medium);
+    test_jsr_abs(&mut usart, &nop, &mut resb);
     test_jmp_abs(&mut usart, &nop, &rw, &mut resb);
     test_jmp_ind(&mut usart, &nop, &rw, &mut resb);
     info!("all tests passed!");
