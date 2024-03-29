a49863aeda5b1c7ca4a750cd561ee72e2a202774
YSawc <niceage6690@gmail.com>
2024-03-29 17:58:37 +0900

    modify: modify bit tests within internal memory.

--- packages/stm32l476rg/src/bin/bit_tests.rs
+++ packages/stm32l476rg/src/bin/bit_tests.rs
@@ -14,6 +14,100 @@ bind_interrupts!(struct Irqs {
     USART1 => usart::InterruptHandler<peripherals::USART1>;
 });
 
+pub fn test_bit_zp_without_flag_within_internal_memory<T: BasicInstance, P: Pin, P2: Pin>(
+    usart: &mut Uart<T>,
+    nop: &Input<P>,
+    resb: &mut Output<P2>,
+) {
+    send_reset_signal_if_not_nop(&nop, resb);
+    usart_write(usart, &[CpuMode::DebugWithinInternalMemory as u8]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xA9, 0x02]);
+    check_valid_register_status(usart, TxReg::A, &[0x02]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000000]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0x8D, 0x01, 0x00]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xA9, 0x3A]);
+    check_valid_register_status(usart, TxReg::A, &[0x3A]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000000]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0x24, 0x01]);
+    check_valid_register_status(usart, TxReg::A, &[0x3A]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000000]);
+    info!("test_bit_zp_without_flag_within_internal_memory passed!");
+}
+
+pub fn test_bit_zp_with_n_within_internal_memory<T: BasicInstance, P: Pin, P2: Pin>(
+    usart: &mut Uart<T>,
+    nop: &Input<P>,
+    resb: &mut Output<P2>,
+) {
+    send_reset_signal_if_not_nop(&nop, resb);
+    usart_write(usart, &[CpuMode::DebugWithinInternalMemory as u8]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xA9, 0x80]);
+    check_valid_register_status(usart, TxReg::A, &[0x80]);
+    check_valid_register_status(usart, TxReg::P, &[0b10000000]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0x8D, 0x01, 0x00]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xA9, 0x80]);
+    check_valid_register_status(usart, TxReg::A, &[0x80]);
+    check_valid_register_status(usart, TxReg::P, &[0b10000000]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0x24, 0x01]);
+    check_valid_register_status(usart, TxReg::A, &[0x80]);
+    check_valid_register_status(usart, TxReg::P, &[0b10000000]);
+    info!("test_bit_zp_with_n_within_internal_memory passed!");
+}
+
+pub fn test_bit_zp_with_nv_within_internal_memory<T: BasicInstance, P: Pin, P2: Pin>(
+    usart: &mut Uart<T>,
+    nop: &Input<P>,
+    resb: &mut Output<P2>,
+) {
+    send_reset_signal_if_not_nop(&nop, resb);
+    usart_write(usart, &[CpuMode::DebugWithinInternalMemory as u8]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xA9, 0xFF]);
+    check_valid_register_status(usart, TxReg::A, &[0xFF]);
+    check_valid_register_status(usart, TxReg::P, &[0b10000000]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0x8D, 0x01, 0x00]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xA9, 0xFF]);
+    check_valid_register_status(usart, TxReg::A, &[0xFF]);
+    check_valid_register_status(usart, TxReg::P, &[0b10000000]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0x24, 0x01]);
+    check_valid_register_status(usart, TxReg::A, &[0xFF]);
+    check_valid_register_status(usart, TxReg::P, &[0b11000000]);
+    info!("test_bit_zp_with_nv_within_internal_memory passed!");
+}
+
+pub fn test_bit_zp_with_z_within_internal_memory<T: BasicInstance, P: Pin, P2: Pin>(
+    usart: &mut Uart<T>,
+    nop: &Input<P>,
+    resb: &mut Output<P2>,
+) {
+    send_reset_signal_if_not_nop(&nop, resb);
+    usart_write(usart, &[CpuMode::DebugWithinInternalMemory as u8]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0x8D, 0x01, 0x00]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0x24, 0x01]);
+    check_valid_register_status(usart, TxReg::A, &[0x00]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000010]);
+    info!("test_bit_zp_with_z_within_internal_memory passed!");
+}
+
+pub fn test_bit_abs_within_internal_memory<T: BasicInstance, P: Pin, P2: Pin>(
+    usart: &mut Uart<T>,
+    nop: &Input<P>,
+    resb: &mut Output<P2>,
+) {
+    send_reset_signal_if_not_nop(&nop, resb);
+    usart_write(usart, &[CpuMode::DebugWithinInternalMemory as u8]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xA9, 0xC0]);
+    check_valid_register_status(usart, TxReg::A, &[0xC0]);
+    check_valid_register_status(usart, TxReg::P, &[0b10000000]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0x8D, 0x78, 0x06]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0xA9, 0x05]);
+    check_valid_register_status(usart, TxReg::A, &[0x05]);
+    check_valid_register_status(usart, TxReg::P, &[0b00000000]);
+    usart_write(usart, &[OpeMode::Inst as u8, 0x2C, 0x78, 0x06]);
+    check_valid_register_status(usart, TxReg::A, &[0x05]);
+    check_valid_register_status(usart, TxReg::P, &[0b11000010]);
+    info!("test_bit_abs_within_internal_memory passed!");
+}
+
 pub fn test_bit_zp_without_flag_within_mocking_memory<T: BasicInstance, P: Pin, P2: Pin>(
     usart: &mut Uart<T>,
     nop: &Input<P>,
@@ -112,10 +206,15 @@ fn main() -> ! {
     let _rw = Input::new(p.PA0, Pull::None);
     let nop = Input::new(p.PA1, Pull::None);
     let mut resb = Output::new(p.PA4, Level::Low, Speed::Medium);
+    test_bit_zp_without_flag_within_internal_memory(&mut usart, &nop, &mut resb);
+    test_bit_zp_with_n_within_internal_memory(&mut usart, &nop, &mut resb);
+    test_bit_zp_with_nv_within_internal_memory(&mut usart, &nop, &mut resb);
+    test_bit_zp_with_z_within_internal_memory(&mut usart, &nop, &mut resb);
+    test_bit_abs_within_internal_memory(&mut usart, &nop, &mut resb);
+
     test_bit_zp_without_flag_within_mocking_memory(&mut usart, &nop, &mut resb);
     test_bit_zp_with_n_within_mocking_memory(&mut usart, &nop, &mut resb);
     test_bit_zp_with_nv_within_mocking_memory(&mut usart, &nop, &mut resb);
-    test_bit_zp_with_z_within_mocking_memory(&mut usart, &nop, &mut resb);
     test_bit_zp_with_z_within_mocking_memory(&mut usart, &nop, &mut resb);
     test_bit_abs_within_mocking_memory(&mut usart, &nop, &mut resb);
     info!("all tests passed!");
